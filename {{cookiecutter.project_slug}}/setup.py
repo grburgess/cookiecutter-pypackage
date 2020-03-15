cad89c210a105e3ef7fb6dc64d0ab671ae2fc958
grburgess <jmichaelburgess@gmail.com>
2020-03-15 08:25:46 +0100

    update my cookie cutter

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,146 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""The setup script."""
-
-
-from setuptools.command.build_ext import build_ext as _build_ext
-from Cython.Build import cythonize
-from setuptools import setup, find_packages, Command, Extension
-import os
-import io
-import sys
-from shutil import rmtree
+from setuptools import setup
+import versioneer
 
 
+# Create list of data files
+def find_data_files(directory):
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
+    paths = []
 
-    description = "Build and publish the package."
-    user_options = []
+    for (path, directories, filenames) in os.walk(directory):
 
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
+        for filename in filenames:
 
-    def initialize_options(self):
-        pass
+            paths.append(os.path.join("..", path, filename))
 
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds...")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution...")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPI via Twine...")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags...")
-        os.system("git tag v{0}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
-
-
-
-# Package meta-data.
-NAME = "{{ cookiecutter.project_slug }}"
-DESCRIPTION = "{{ cookiecutter.project_short_description }}"
-URL = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}'
-EMAIL = '{{ cookiecutter.email }}'
-AUTHOR = "{{ cookiecutter.full_name.replace('\"', '\\\"') }}"
-REQUIRES_PYTHON = ">=2.7.0"
-VERSION = None
-
-REQUIRED = [
-    "numpy",
-    "scipy",
-    "ipython",
-    "matplotlib",
-    "h5py",
-    "pandas",
-    "Cython",
-
-]
-
-
+    return paths
 
 
 #extra_files = find_data_files("{{ cookiecutter.project_slug }}/data")
 
-
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = REQUIRED
-
-setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %} ]
-
-test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest',{%- endif %} ]
-
-{%- set license_classifiers = {
-    'MIT license': 'License :: OSI Approved :: MIT License',
-    'BSD license': 'License :: OSI Approved :: BSD License',
-    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
-    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
-    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
-} %}
-
 setup(
-    author=AUTHOR,
-    author_email=EMAIL,
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-{%- if cookiecutter.open_source_license in license_classifiers %}
-        '{{ license_classifiers[cookiecutter.open_source_license] }}',
-{%- endif %}
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description=DESCRIPTION,
-    {%- if 'no' not in cookiecutter.command_line_interface|lower %}
-    entry_points={
-        'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
-        ],
-    },
-    {%- endif %}
-    install_requires=requirements,
-{%- if cookiecutter.open_source_license in license_classifiers %}
-    license="{{ cookiecutter.open_source_license }}",
-{%- endif %}
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='{{ cookiecutter.project_slug }}',
-    name='{{ cookiecutter.project_slug }}',
-    packages=find_packages(include=['{{ cookiecutter.project_slug }}'],exclude=('tests')),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
-    version='{{ cookiecutter.version }}',
-    zip_safe=False,
-    cmdclass={"upload": UploadCommand},
+
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 #        package_data={"": extra_files},
 )
 
